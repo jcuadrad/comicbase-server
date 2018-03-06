6c4303d15d35eba6b3f865a9b8673a9c9f42426f
Juan Cuadra <jcb8793@gmail.com>
2018-03-06 00:00:29 -0600

    Create Comics Post Route

--- routes/comics.js
+++ routes/comics.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+// Model Import
+const Comic = require('../models/Comic').Comic;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', (req, res, next) => {
+  const newComic = new Comic({
+    name: req.body.name,
+    writer: req.body.writer,
+    artist: req.body.artist,
+    publisher: req.body.publisher,
+    volume: req.body.volume,
+    cover: req.body.cover,
+  });
+
+  newComic.save(error => {
+    if (error) {
+      res.status(500).json({status: 'Something got fucked up'});
+    } else {
+      res.status(200).json({status: 'Saved!'})
+    }
+  })
+  .catch(err => {
+    next(err);
+  })
+})
 
 module.exports = router;
